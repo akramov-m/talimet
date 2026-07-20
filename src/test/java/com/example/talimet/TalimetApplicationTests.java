5cd7afffd98f6faca230c8d6a50a8f974ced4705
m-ravshanovch <muhammadqodir.tr@gmail.com>
2026-07-20 21:39:14 +0500

    start

---
+++ src/test/java/com/example/talimet/TalimetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talimet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalimetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
